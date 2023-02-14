96e50826553977356c0b66ef630f92efb7c58e2b
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-02-14 12:00:29 -0500

    Added basic script functionality

--- library.js
+++ library.js
@@ -1,2 +1,26 @@
 "use strict"
 
+const form = document.getElementById('book-submission');
+
+
+form.addEventListener("submit", addBookToLibrary, true);
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(event) {
+  event.preventDefault();
+  let formData = new FormData(event.target);
+  let title = formData.get('title');
+  let author = formData.get('author');
+  let numberOfPages = formData.get('number-of-pages');
+  let readStatus = formData.get('read-status');
+  let newBook = new Book(title, author, numberOfPages, readStatus);
+  myLibrary.push(newBook);
+}
